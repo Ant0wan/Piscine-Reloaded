5b16f0adb8975771e90d6ff2b7f303a4567ae5f8
Antoine BARTHELEMY <abarthel@e3r12p2.42.fr>
2018-11-05 19:24:46 +0100

    ex27

--- ex27/srcs/display_file.c
+++ ex27/srcs/display_file.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 19:07:06 by abarthel          #+#    #+#             */
-/*   Updated: 2018/11/05 19:17:42 by abarthel         ###   ########.fr       */
+/*   Updated: 2018/11/05 19:24:23 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,44 @@ void	ft_putstr(char *c)
 	}
 }
 
+int		ft_display(char **argv)
+{
+	int		fd;
+	int		ret;
+	char	buf[BUF_SIZE + 1];
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr("Open failed\n");
+		return (1);
+	}
+	ret = read(fd, buf, BUF_SIZE);
+	while (ret)
+	{
+		buf[ret] = '\0';
+		ft_putstr(buf);
+		ret = read(fd, buf, BUF_SIZE);
+	}
+	if (close(fd) == -1)
+	{
+		ft_putstr("Close failed\n");
+		return (1);
+	}
+	return (0);
+}
+
 int		main(int argc, char **argv)
 {
 	if (argc < 2)
 	{
 		ft_putstr("File name missing.\n");
 		return (1);
+	}
+	if (argc == 2)
+	{
+		if (ft_display(argv) != 0)
+			return (1);
 	}
 	if (argc > 2)
 	{
